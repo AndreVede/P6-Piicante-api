e8a1d4dcecbcffd4dd968c7e9266d7cde78bf0d0
Zacharie Boisnard <mecazack@gmail.com>
2023-01-27 15:39:31 +0100

    feat: création du serveur

---
+++ server.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const app = require('./app');
+
+// Traite le port et vérifie sa conformité s'il vient d'une variable d'environnement
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+
+// Si une variable d'environnement n'est pas spécifiée, c'est le port 3000 qui sera utilisé
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * capture les erreurs du serveur
+ * @param {any} error
+ */
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind =
+        typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+// démarrage du serveur
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind =
+        typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+});
+
+server.listen(port);
