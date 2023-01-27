2f67efd7584e6fc16d817c144e141a135b3d3316
Zacharie Boisnard <mecazack@gmail.com>
2023-01-27 16:33:42 +0100

    feat: accès mongoDB

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// accès mongoDB
+const mongodbClient = {
+    username: 'piiquante-user',
+    password:
+        '73pu2zEfKZ34cSJiU352vxJSHF5PqzfkaDva6RaEdu5Ew9T6H6j8Eb7Yqqr7sgNf9GakZK',
+};
+const uri = `mongodb+srv://${mongodbClient.username}:${mongodbClient.password}@piiquante-api.mongodb.net/piiquante-api?retryWrites=true&w=majority`;
+try {
+    mongoose.connect(uri, () => {
+        console.log('MongoDB is connected !');
+    });
+} catch (error) {
+    console.error('MongoDB not connected !');
+}
+
+//initialisation d'express
 const app = express();
+app.use(express.json());
 
 module.exports = app;
